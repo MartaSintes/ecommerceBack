966260370b2e47c0fae3d25ef118aed7581850ac
MartaSintes <78809511+MartaSintes@users.noreply.github.com>
2024-05-18 22:10:46 +0200

    Feat(edit)

--- src/api/usuario/usuario.controller.ts
+++ src/api/usuario/usuario.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get, Post, Req, Res, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { UsuarioService } from './usuario.service';
 import { AuthGuard } from 'src/guards/auth/auth.guard';
 
@@ -19,6 +29,26 @@ export class UsuarioController {
   async getUsuarios(@Res() res) {
     const usuarios = await this.usuarioService.getUsuarios();
     res.status(200).send(usuarios);
+  }
+  @Get('getUsuario/:id')
+  @UseGuards(AuthGuard)
+  async getUsuario(@Res() res, @Req() req, @Param('id') id: any) {
+    const usuario = await this.usuarioService.getUsuario(id);
+    res.status(200).send(usuario);
+  }
+
+  @Put('updateUsuario/:id')
+  @UseGuards(AuthGuard)
+  async updateUsuario(@Res() res, @Req() req, @Param('id') id: any) {
+    const data = req.body;
+    const usuario = await this.usuarioService.updateUsuario(id, data);
+    res.status(200).send(usuario);
+  }
+  @Delete('deleteUsuario/:id')
+  @UseGuards(AuthGuard)
+  async deleteUsuario(@Res() res, @Param('id') id: any) {
+    const result = await this.usuarioService.deleteUsuario(id);
+    res.status(200).send(result);
   }
 
   @Post('login')
