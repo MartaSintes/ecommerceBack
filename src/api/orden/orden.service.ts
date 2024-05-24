2eaf8e458d2db7af79c6692fc9aa102f05590422
MartaSintes <78809511+MartaSintes@users.noreply.github.com>
2024-05-24 10:12:02 +0200

    feat(order)

---
+++ src/api/orden/orden.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+
+@Injectable()
+export class OrdenService {
+  constructor(@InjectModel('ordenes') private ordenModel) {}
+
+  async createOrden(data: any) {
+    try {
+      const orden = await this.ordenModel.create(data);
+      return { data: orden };
+    } catch (error) {
+      return { data: undefined, message: 'No se pudo crear el orden' };
+    }
+  }
+
+  async findOrdenesUsuario(usuarioId: string) {
+    try {
+      const ordenes = await this.ordenModel
+        .find({ usuario: usuarioId })
+        .populate('producto');
+
+      return { data: ordenes };
+    } catch (error) {
+      return {
+        data: undefined,
+        message: 'No se pudieron obtener las órdenes del usuario',
+      };
+    }
+  }
+}
