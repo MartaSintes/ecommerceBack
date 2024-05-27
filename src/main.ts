ad3e4ba6a3b4e7173935ffad733b2c503303c08a
MartaSintes <78809511+MartaSintes@users.noreply.github.com>
2024-05-27 12:17:01 +0200

    port

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  const port = process.env.PORT;
+  const port = process.env.PORT ?? 3000;
+  console.log(`Server running on port ${port}`);
   await app.listen(port);
 }
 bootstrap();
